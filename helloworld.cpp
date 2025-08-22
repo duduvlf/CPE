c7f35dc673e7a4a08b12645d0856572d006117ac
isa123 <isadorarfigueira@gmail.com>
2025-08-22 11:32:16 -0300

    ok

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+//Este es un programa en C++ que imprime "Hello, World!" en la consola.
+//data
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+
+    system("pause");
+
+    return 0;
+}
