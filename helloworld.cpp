bc0e519fd41b5728bd985dc6a8823698364b07c7
dudu <beadieroak98164@gmail.com>
2025-08-27 11:51:42 -0300

    aula27/08

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,13 +1,18 @@
 //Este es un programa en C++ que imprime "Hello, World!" en la consola.
 //data
 
-
+// using namespace std; - sempre acima do int main - serve p n ficar dando std::cout
+// g++ helloworld.cpp -o helloworld -- cria o executável helloworld
+// Uma lógica && (E) em C++ é representada por "&&"
 #include <iostream>
+#include <cstdlib> // Necessário para a função system
 
 int main() {
+    
     std::cout << "Hello, World!\n";
 
+    system("color 0A"); // Muda a cor do texto para verde no Windows
+    system("cls"); // Limpa a tela no Windows - se estiver usando o Hello World não aparece
     system("pause");
-git checkout -b main
     return 0;
-}
+}    
