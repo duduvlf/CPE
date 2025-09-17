2740ba0b64d2ed020915da885f245a86ef97203a
dudu <beadieroak98164@gmail.com>
2025-09-17 10:12:42 -0300

    forca

--- forca.cpp
+++ forca.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-#include <ctime>
 
 using namespace std;
     int main () {
@@ -13,35 +12,38 @@ using namespace std;
 system ("cls");
 
     int qntdel = palavra.length ();
-    cout << " A palavra tem " << qntdl << " letras " << endl;
+    cout << " A palavra tem " << qntdel << " letras " << endl;
 
-    while  (true) { // começa o loop
-        string chute; // armazena palavra   
+   
+        string chute; // armazena palavra 
+        int acertos = 0; // conta acertos  
         cout << " Vamos começar o jogo " << endl;
+         while  (true) { // começa o loop
         cout << " Digite uma letra: ";
         cin >> chute;
-        chute=tolower(chute); // converte para minusculo
+       chute[0] = tolower(chute[0]);
         bool tem = false; // variavel de controle
-        for (int i=0; i < qntdl; i++) {
-            if (chute [0] == palavra [i]) {
+        for (int i=0; i < qntdel; i++) { // verifica letra por letra
+            if (chute[0] == palavra[i]) {
                 tem = true; // se a letra estiver na palavra
-                cout << " A letra " << chute << " está na palavra " << endl;
-                break; // sai do for
-                else {
-                    cout << " A letra " << chute << " não está na palavra " << endl;
-                }
+                acertos++; // conta o acerto
             }
+         }  // loop for, verifica letra por letra
+         if (tem) {
 
-
-            }
-
-
-
-
-            
-        } 
-        i++;
+cout << " Acertou uma letra! " << endl;
+cout << " Você já acertou " << acertos << " letras. " <<endl;
+if (qntdel - acertos > 0) {
+cout << " Ainda restam: " << qntdel - acertos << " letras "<< endl; 
+ } } else {
+cout << " Errou! Tente novamente " << endl;
+cout << " Você já acertou " << acertos << " letras " << "Ainda restam:" << qntdel - acertos <<endl; 
+                 }
+        if (acertos == qntdel) { // se acertar todas as letras
+            cout << " Parabéns! Você ganhou! A palavra era " << palavra << endl;
+            break; // sai do loop
+                   } } 
+        system ("pause");
+        return 0;
 
     }
-
-    
